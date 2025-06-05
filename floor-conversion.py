4d37289149636a22e979ba1dda8c63ac36be813b
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-06-05 23:02:25 +0700

    initial commit

---
+++ floor-conversion.py
@@ -0,0 +1,6 @@
+# This script helps convert the elevator floor number
+# from European standard to the US standard.
+
+eu_floor = input('Please input the current European floor number: ')
+us_floor = int(eu_floor) + 1
+print('The corresponding US floor number is:', us_floor)
