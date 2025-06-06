0a20c5a38e3b79688d3af73fa2e0ec003d8118a2
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-06-06 16:46:53 +0700

    prepare for gross-pay-2.py

---
+++ gross-pay-2.py
@@ -0,0 +1,3 @@
+# This script prompts the user to provide their number of hours worked
+# and the rate per hour, then calculates the total salary, with
+# the ratio multiplied by 1.5 for an additional hour out of 40 hours.
