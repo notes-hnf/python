52a7fca1025bd1f46c6f789f6ded09c6a8cac7a1
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-06-13 16:41:14 +0700

    finished file-read-print.py

---
+++ file-read-print.py
@@ -0,0 +1,13 @@
+# This script prompts the user to provide a filename,
+# then reads through and prints the content of the file
+# (line by line) all in upper case.
+
+file_name = input('Please enter a filename: ')
+try:
+    file_handle = open(file_name)
+except:
+    print('No file found with the given filename!')
+    quit()
+for line in file_handle:
+    line_beautified = line.rstrip().upper()
+    print(line_beautified)
